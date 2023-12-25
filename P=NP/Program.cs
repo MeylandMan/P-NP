4861937ad9844498c79976954cbb151a421a4b63
MeylandMan <125752061+MeylandMan@users.noreply.github.com>
2023-12-25 12:48:16 +0100

    Little setup

--- P=NP/Program.cs
+++ P=NP/Program.cs
@@ -2,10 +2,37 @@
 
 class Program
 {
-
     static void Main(string[] args) {
+        int item_n = 8;
+        int[,] Knapsack = {
+                {50}, // Knapsack weight capacity
+                {10000} // Knapsack price capacity
+        };
+
+        int[][][] items = new int[item_n][item_n][item_n];
 
         Console.WriteLine("Hello World !");
+
+    }
+
+    public static void item_list(int[][][]args) {
+        enum Item_name {
+            Piano, 
+            Synthetiseur, 
+            MacBook, 
+            Guitar, 
+            Speaker, 
+            PetitPiano, 
+            Table, 
+            Chaise,
+        }
+
+        int[][][] array = {
+                {Item_name.Piano, Item_name.Synthetiseur, Item_name.MacBook, Item_name.Guitar, Item_name.Speaker, Item_name.PetitPiano, Item_name.Table, Item_name.Chaise}, // Item ID
+                {2000,           500,                    2400,              1000,             1200,              2000,                 4000,            3000}, // Item PRICE
+                {12,             4,                      12,                3,                8,                 3,                    23,              20} // Item Weight
+        };
+        args = array;
 
     }
 
--- P=NP/obj/Debug/net8.0/P=NP.AssemblyInfo.cs
+++ P=NP/obj/Debug/net8.0/P=NP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P=NP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735db96cebcfd2cceeb6d6717e7eec937635970c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6282ca3f31241026ab575a08e35c3f407a83077b")]
 [assembly: System.Reflection.AssemblyProductAttribute("P=NP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P=NP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
